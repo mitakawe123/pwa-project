f199cf091a6dae1c722b390f3c128581ab29100c
mitakawe123 <dkyuchukov2003@gmail.com>
2022-08-19 11:04:16 +0300

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbModal,
+  NgbModalConfig,
+  NgbActiveModal,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ServiceWorkerModule, NgbModalConfig, NgbModal],
+  providers: [ServiceWorkerModule, NgbModalConfig, NgbModal, NgbActiveModal],
 })
 export class AppComponent implements AfterViewInit {
   title = 'pwa-project';
@@ -14,7 +18,11 @@ export class AppComponent implements AfterViewInit {
 
   @ViewChild('mymodal') mymodal: ElementRef | undefined;
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+  constructor(
+    config: NgbModalConfig,
+    private modalService: NgbModal,
+    private activeModal: NgbActiveModal
+  ) {
     // customize default values of modals used by this component tree
     config.backdrop = 'static';
     config.keyboard = false;
@@ -23,6 +31,8 @@ export class AppComponent implements AfterViewInit {
   onSubmit(): void {
     this.isPrompt = true;
     if (this.isPrompt === true) {
+      this.activeModal.close();
+      // this.modalService.close();
       navigator.serviceWorker.register('ngsw-worker.js');
     }
   }
